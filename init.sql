2709a08f8aa671afddfb574f6c61ad1d269ad669
miss-sylph <85602451+miss-sylph@users.noreply.github.com>
2025-01-22 21:33:58 +1000

    create

---
+++ init.sql
@@ -0,0 +1,28 @@
+CREATE TABLE test_table (
+    name VARCHAR(10) NOT NULL CHECK (LENGTH(name) BETWEEN 4 AND 10),
+    surname TEXT NOT NULL,
+    city TEXT NOT NULL,
+    age INT NOT NULL CHECK (age > 0 AND age <= 150)
+);
+
+INSERT INTO test_table (name, surname, city, age) VALUES
+('Ivan', 'Ivanov', 'Moscow', 25),
+('Anna', 'Petrova', 'Saint Peterburg', 35),
+('Sergei', 'Sidorov', 'Novosibirsk', 30),
+('Maria', 'Vasileva', 'Kazan', 42),
+('Alexander', 'Fedorov', 'Ekaterinburg', 28),
+('Elena', 'Smirnova', 'Sochi', 33),
+('Nikita', 'Orlov', 'Voronezh', 24),
+('Tatiana', 'Kuznetsova', 'Samara', 39),
+('Andrey', 'Morozov', 'Rostov-on-Don', 27),
+('Olga', 'Stepanova', 'Ufa', 40),
+('Vladimir', 'Popov', 'Krasnodar', 29),
+('Natalia', 'Kozlova', 'Perm', 22),
+('Yevgeniy', 'Romanov', 'Volgograd', 34),
+('Irina', 'Sokolova', 'Saratov', 31),
+('Maksim', 'Lebedev', 'Krasnoyarsk', 37),
+('Victor', 'Pavlov', 'Tula', 36),
+('Lidiya', 'Vasilieva', 'Izhevsk', 26),
+('Konstantin', 'Semenov', 'Chelyabinsk', 38),
+('Galina', 'Aleksandrova', 'Tomsk', 41),
+('Valentina', 'Grigorieva', 'Barnaul', 43);
